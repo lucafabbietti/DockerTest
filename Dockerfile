0c1fcadb831b2628abdd5cac5948772e2a512f6f
Luca Fabbietti <lucafabbietti@yahoo.it>
2022-01-18 19:36:30 +0100

    New Attempt

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
 FROM ubuntu:20.04
+ENV TZ=Europe/Rome
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 RUN apt-get update && apt-get install -y dialog apt-utils apache2 php libapache2-mod-php
+RUN echo "ServerName localhost" >> /etc/apache2/apache2.conf
+WORKDIR /var/www/html
+COPY index.php index.php
 EXPOSE 80
+RUN apachectl start
 
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Orario attuale:
+        <?php
+        echo date("D, d M Y H:i:s");
+        ?>
+    </h1>
+
+</body>
+
+</html>
